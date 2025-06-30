8b332d02bd3536dd27889d53bff54c3af4790f9c
Titus Schell <titus.schell@gmail.com>
2025-06-30 14:49:24 +0200

    Improved Formatting

--- dinoscript.js
+++ dinoscript.js
@@ -1,30 +1,30 @@
- const dino = document.getElementById("dino");
- const cactus = document.getElementById("cactus")
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus")
 
- function jump() {
-    if (dino.classList != "jump"){
-    dino.classList.add("jump");
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
 
-    setTimeout(function(){
-        dino.classList.remove("jump");
-    }, 400);
+        setTimeout(function () {
+            dino.classList.remove("jump");
+        }, 400);
+    }
 }
- }
 
- document.addEventListener("keydown", function (event)  {
+document.addEventListener("keydown", function (event) {
     jump();
- });
+});
 
- let isAlive = setInterval(function ()  {
+let isAlive = setInterval(function () {
     // get current dino Y position
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     // get current cactus X position
-let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-// detect collision
-if (cactusLeft <50 && cactusLeft > 0 && dinoTop >= 140){
-// collision
-alert("YOU LOST. LOSER!!!")
-}
+    // detect collision
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+        // collision
+        alert("YOU LOST. LOSER!!!")
+    }
 }, 10);
 
