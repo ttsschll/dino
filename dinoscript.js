c0b95d77a66cf7ce3eb242eef866a33ae19343b6
Titus Schell <titus.schell@gmail.com>
2025-07-01 12:56:49 +0200

    Including Scores

--- dinoscript.js
+++ dinoscript.js
@@ -1,5 +1,8 @@
 const dino = document.getElementById("dino");
-const cactus = document.getElementById("cactus")
+const cactus = document.getElementById("cactus");
+const points = document.getElementById("points");
+const highscore = document.getElementById("highscorevalue");
+let score = 0;
 
 function jump() {
     if (dino.classList != "jump") {
@@ -21,10 +24,21 @@ let isAlive = setInterval(function () {
     // get current cactus X position
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
+    var highscoreValue = localStorage.getItem("highscore");
+    points.innerText = score++;
+    highscore.innerText = highscoreValue;
+
+    if (score > highscoreValue) {
+        localStorage.setItem("highscore", score);
+    }
+
+
     // detect collision
     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
         // collision
-        alert("YOU LOST. LOSER!!!")
+        alert("YOU LOST!");
+        window.location.reload();
+        //if(confirm("YOU LOST!")) document.location.reload();
     }
 }, 10);
 
